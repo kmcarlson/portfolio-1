5a6b163e8fe872f015eb79061652a57ee3edb48c
Kenneth Carlson <carlson90@hotmail.com>
2022-10-05 10:06:42 +0200

    Add go top js

--- javascript/script.js
+++ javascript/script.js
@@ -23,3 +23,25 @@ menuShow.addEventListener('click', () => {
     
 // }
 
+
+// Get the button
+const goToHome = document.getElementById("buttonTop");
+
+
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 10 || document.documentElement.scrollTop > 10) {
+//     goToHome.style.display = "block";
+//   } else {
+//     goToHome.style.display = "none";
+//   }
+// }
+
+
+//Scroll to top
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
